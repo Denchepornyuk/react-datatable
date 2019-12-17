692fde08dfe05b11186acddffa506e416c1135c3
Denys <denys.cheporniuk@gmail.com>
2019-12-17 19:37:02 +0200

    Updates

---
+++ precache-manifest.7f1d83666179840d59add2a2ca39c84a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b28177ce4dc737099bab09fc93567fb",
+    "url": "/react-datatable/index.html"
+  },
+  {
+    "revision": "930d5b65ab312c443615",
+    "url": "/react-datatable/static/css/main.e0df6163.chunk.css"
+  },
+  {
+    "revision": "ccee7f00b43c18165256",
+    "url": "/react-datatable/static/js/2.865935c1.chunk.js"
+  },
+  {
+    "revision": "930d5b65ab312c443615",
+    "url": "/react-datatable/static/js/main.fea0b937.chunk.js"
+  },
+  {
+    "revision": "4a7f270f323948ec8a56",
+    "url": "/react-datatable/static/js/runtime~main.22478f67.js"
+  }
+]);
